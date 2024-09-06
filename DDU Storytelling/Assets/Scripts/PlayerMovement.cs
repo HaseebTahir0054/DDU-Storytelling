1541ba7cf435f2ad7c8afab497184b24e25254fe
HaseebTahir0054 <mian.m.haseeb0054@gmail.com>
2024-09-07 00:30:56 +0200

    kig i retning
    Opdateret koden, så spilleren kigger i den retning den går imod.

--- DDU Storytelling/Assets/Scripts/PlayerMovement.cs
+++ DDU Storytelling/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
-    [SerializeField]private float speed;
+    [SerializeField]private float speed;  //Gøre sådan så at jeg kan ændre spilleren hastighed i Unity 
 
     private void Awake()
     {
@@ -12,11 +12,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
-        if (Input.GetKey(KeyCode.W))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);  // Gør så spilleren bevæger sig højre eller vestre 
+
+
+        if (horizontalInput > 0)
+            transform.localScale = Vector3.one;
+
+        else if (horizontalInput < 0)
+            transform.localScale = new Vector3(-1,1,1);      // Gøre sådan så at spillerens sprite kigger den retning man bevæger sig imod
 
         if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+            body.velocity = new Vector2(body.velocity.x, speed);  // Spilleren hopper hvis man trykker Space//
+
+        if (Input.GetKey(KeyCode.W))
+            body.velocity = new Vector2(body.velocity.x, speed);   // Spiller hopper hvis man trykker W //
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            body.velocity = new Vector2(body.velocity.x, speed);   // Spiller hopper hvis man trykker Up knap //
     }
 } 
